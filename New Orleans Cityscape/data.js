f9909bb1021f1fa566300d3ff34c80a2fbdd45c1
unitythemovement <95473103+unitythemovement@users.noreply.github.com>
2025-11-21 05:00:47 -0600

    Initial Project Upload

---
+++ New Orleans Cityscape/data.js
@@ -0,0 +1,230 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sunrise-over-nola---riverwalk-360",
+      "name": "Sunrise over NOLA - Riverwalk 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -1.219519661700481,
+        "pitch": 0.26842699010601123,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.997901004256951,
+          "pitch": 0.02876451237494848,
+          "title": "Crescent City Connection",
+          "text": "Formerly the Greater New Orleans Bridge, The Crescent City Connection is a pair of cantilever bridges that carry U.S. Highway 90 Business over the Mississippi River in New Orleans, USA.&nbsp; They are tied as the fifth-longest cantilever bridges in the world.&nbsp;"
+        },
+        {
+          "yaw": 0.052243358192662015,
+          "pitch": 0.372629696672238,
+          "title": "Canal Street",
+          "text": "<span class=\"N9Q8Lc\" style=\"border-radius: 0px; background-color: rgb(44, 48, 61); color: rgb(230, 232, 240); font-family: &quot;Google Sans&quot;, Roboto, Arial, sans-serif; font-size: 16px;\">Canal Street is&nbsp;</span><mark class=\"HxTRcb\" jscontroller=\"DfH0l\" jsuid=\"qkHoUc_b\" style=\"color: rgb(238, 240, 255); border-radius: 0px; background: linear-gradient(90deg, rgb(52, 69, 127) 50%, rgba(0, 0, 0, 0) 50%) 75% 0px / 200% 100% no-repeat scroll padding-box border-box rgb(44, 48, 61); padding: 0px 2px; animation: 0.75s cubic-bezier(0.05, 0.7, 0.1, 1) 0.25s 1 normal forwards running highlight-animation; font-family: &quot;Google Sans&quot;, Roboto, Arial, sans-serif; font-size: 16px;\"><span class=\"N9Q8Lc\" style=\"border-radius: 0px;\">a major thoroughfare in New Orleans that serves as a historic dividing line between the French Quarter and the American Sector.&nbsp;</span></mark><span class=\"N9Q8Lc\" style=\"border-radius: 0px; background-color: rgb(44, 48, 61); color: rgb(230, 232, 240); font-family: &quot;Google Sans&quot;, Roboto, Arial, sans-serif; font-size: 16px;\">Originally planned as a canal, it became the city's \"neutral ground\" and is now a vibrant entertainment and business district, famous for its historic streetcars that run along the wide median.</span><span jsuid=\"qkHoUc_c\" class=\"uJ19be notranslate\" jsaction=\"rcuQ6b:&amp;qkHoUc_c|npT2md\" jscontroller=\"udAs2b\" data-wiz-uids=\"qkHoUc_c,qkHoUc_d,qkHoUc_e\" style=\"color: rgb(230, 232, 240); font-family: &quot;Google Sans&quot;, Roboto, Arial, sans-serif; font-size: 16px; background-color: rgb(31, 31, 31);\"><span class=\"vKEkVd\" data-animation-atomic=\"\" style=\"text-wrap-mode: nowrap; position: relative;\">&nbsp;</span></span>"
+        },
+        {
+          "yaw": 2.517951539861838,
+          "pitch": 0.12293619529608613,
+          "title": "Algiers Point",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-sunset-at-uno-360",
+      "name": "Sunset at UNO 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -0.06945698043650417,
+        "pitch": 0.37400636815482713,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.01618143869970723,
+          "pitch": -0.11448446751398222,
+          "rotation": 3.141592653589793,
+          "target": "0-sunrise-over-nola---riverwalk-360"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.686203093535001,
+          "pitch": 0.20901549061315805,
+          "title": "The University of New Orleans",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.0228927285611515,
+          "pitch": 0.16430160935882654,
+          "title": "Lake Pontchartrain",
+          "text": "<span style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 16px; background-color: rgb(255, 255, 255);\">is an&nbsp;</span><a href=\"https://en.wikipedia.org/wiki/Estuary\" title=\"Estuary\" style=\"color: rgb(51, 102, 204); background: none rgb(255, 255, 255); border-radius: 2px; overflow-wrap: break-word; font-family: sans-serif; font-size: 16px; font-weight: 400;\">estuary</a><span style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 16px; background-color: rgb(255, 255, 255);\">&nbsp;located in southeastern&nbsp;</span><a href=\"https://en.wikipedia.org/wiki/Louisiana\" title=\"Louisiana\" style=\"color: rgb(51, 102, 204); background: none rgb(255, 255, 255); border-radius: 2px; overflow-wrap: break-word; font-family: sans-serif; font-size: 16px; font-weight: 400;\">Louisiana</a><span style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 16px; background-color: rgb(255, 255, 255);\">&nbsp;in the United States. It covers an area of 630 square miles</span><span style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 16px; background-color: rgb(255, 255, 255);\">&nbsp;with an average depth of 12 to 14 feet.&nbsp;&nbsp;</span><span style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 16px; background-color: rgb(255, 255, 255);\">The estuary is crossed by the&nbsp;</span><a href=\"https://en.wikipedia.org/wiki/Lake_Pontchartrain_Causeway\" title=\"Lake Pontchartrain Causeway\" style=\"color: rgb(51, 102, 204); background: none rgb(255, 255, 255); border-radius: 2px; overflow-wrap: break-word; font-family: sans-serif; font-size: 16px; font-weight: 400;\">Lake Pontchartrain Causeway</a><span style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 16px; background-color: rgb(255, 255, 255);\">, the longest continuous bridge over water in the world.</span>"
+        }
+      ]
+    },
+    {
+      "id": "2-sunrise-over-loyola-nola",
+      "name": "Sunrise over Loyola NOLA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.28319024110339086,
+        "pitch": 0.34601949706841495,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8019789366789283,
+          "pitch": -0.13410747581033888,
+          "rotation": 3.141592653589793,
+          "target": "0-sunrise-over-nola---riverwalk-360"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.08993551460973137,
+          "pitch": 0.6350758962245671,
+          "title": "Loyola University New Orleans",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.4840891067404005,
+          "pitch": 0.26510415803516096,
+          "title": "Tulane University",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.6977078451717453,
+          "pitch": 0.18281566740011357,
+          "title": "Audubon Park",
+          "text": "<p class=\"MsoNormal\">At roughly 350 acres,&nbsp; Audubon Park is approximately\nsix miles to the west of the city center of New Orleans.&nbsp; It&nbsp;offers\nfamily fun with playgrounds, jogging paths, tennis courts, and more, all under\nmajestic live oaks.&nbsp;<o:p></o:p></p>"
+        }
+      ]
+    },
+    {
+      "id": "3-city-park-new-orleans",
+      "name": "City Park New Orleans",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -0.013401404516269366,
+        "pitch": 0.08909292495249588,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4172181175036265,
+          "pitch": -0.11548262187743852,
+          "rotation": 3.141592653589793,
+          "target": "0-sunrise-over-nola---riverwalk-360"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.027689155624827322,
+          "pitch": 0.37059610492799777,
+          "title": "New Orleans Museum of Art",
+          "text": "<p class=\"MsoNormal\">New Orleans Museum of Art is the city’s oldest fine arts\ninstitution.&nbsp; It opened on&nbsp;December\n16, 1911&nbsp;with only nine works of art. Today, the museum hosts an\nimpressive permanent collection of more than 40,000 objects. The collection,\nnoted for its extraordinary strengths\nin&nbsp;French&nbsp;and&nbsp;American&nbsp;art, photography, glass, and African\nand&nbsp;Japanese works, continues to expand and grow.<o:p></o:p></p>"
+        }
+      ]
+    }
+  ],
+  "name": "New Orleans Cityscape",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
