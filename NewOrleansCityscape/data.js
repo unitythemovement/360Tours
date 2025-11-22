aa4e2fd05a52cddab6ef9824aefa7a63d92d70c7
unitythemovement <95473103+unitythemovement@users.noreply.github.com>
2025-11-22 07:53:56 -0600

    Update data.js

--- NewOrleansCityscape/data.js
+++ NewOrleansCityscape/data.js
@@ -55,7 +55,7 @@ var APP_DATA = {
         },
 {
   "yaw": 2.01,
-  "pitch": 0.10001,
+  "pitch": 0.09,
   "title": "French Quarter",
   "text": "The French Quarter, or Vieux Carré, is the oldest neighborhood in New Orleans. Known for its historic architecture, live music, and vibrant nightlife, it stretches along the Mississippi River and centers around Jackson Square and the iconic St. Louis Cathedral."
 }
