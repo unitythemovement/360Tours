6dc0ed1721271858d8bdb9955dcea63ad028a65e
unitythemovement <95473103+unitythemovement@users.noreply.github.com>
2026-09-05 08:34:51 -0500

    Add Cincinnati Cityscape Marzipano Tour

---
+++ CincinnatiCityscape/data.js
@@ -0,0 +1,155 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cincinnatidowntown",
+      "name": "CincinnatiDowntown",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -0.006768249656817105,
+        "pitch": 0.19071931923669183,
+        "fov": 1.0323494145903935
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.16097942647213515,
+          "pitch": 0.34703465939547584,
+          "title": "John A. Roebling Suspension Bridge",
+          "text": "<p class=\"MsoNormal\">The John A. Roebling Suspension Bridge is a suspension\nbridge that spans the Ohio River between Cincinnati, Ohio, and Covington,\nKentucky. When opened to traffic on January 1, 1867, it was the longest\nsuspension bridge in the world at 1,057 feet, before being overtaken by the\nNiagara Clifton Bridge, which was later surpassed by John A. Roebling's most\nfamous design of the 1883 Brooklyn Bridge at 1,595.5 feet. Pedestrians use the\nbridge to get between the hotels, bars, restaurants, and other amenities in Northern\nKentucky. The bar and restaurant district at the foot of the bridge on the\nKentucky side is known as Roebling Point.<o:p></o:p></p>"
+        },
+        {
+          "yaw": -0.6589936517210244,
+          "pitch": 0.1668386562184807,
+          "title": "Paycor Stadium",
+          "text": "<p class=\"MsoNormal\">Originally named after the Bengals' founder, Paul Brown, the\nstadium is currently sponsored by Paycor, is located on approximately 22 acres of\nland, and has a listed seating capacity of 65,515. The stadium is nicknamed\n\"The Jungle\"; the Guns N' Roses song \"Welcome to the\nJungle\", is the team's unofficial anthem due in part to the nickname.&nbsp; Opened on August 19, 2000.<o:p></o:p></p>"
+        },
+        {
+          "yaw": 0.4965903461017085,
+          "pitch": 0.15848754048402292,
+          "title": "Great American Ball Park",
+          "text": "<p class=\"MsoNormal\">Located on the winding banks of the Ohio River in downtown\nCincinnati, Great American Ball Park serves as the home of the Cincinnati Reds,\nbaseball's first professional franchise.<o:p></o:p></p>"
+        }
+      ]
+    },
+    {
+      "id": "1-cincinnatimusichall",
+      "name": "CincinnatiMusicHall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.0612612550211189,
+        "pitch": 0.2246942727920942,
+        "fov": 0.7989227156117577
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.019548951581748852,
+          "pitch": 0.4246353964105616,
+          "title": "Cincinnati Music Hall",
+          "text": "<p class=\"MsoNormal\">Cincinnati Music Hall, is a concert hall in Cincinnati,\nOhio, United States, completed in 1878. It serves as the home for the\nCincinnati Ballet, Cincinnati Symphony Orchestra, Cincinnati Opera, May\nFestival Chorus, and the Cincinnati Pops Orchestra. In January 1975, it was\nrecognized as a National Historic Landmark by the U.S. Department of the\nInterior for its distinctive Venetian Gothic architecture.<o:p></o:p></p>"
+        },
+        {
+          "yaw": 0.6118457183490129,
+          "pitch": 0.1298882199001241,
+          "title": "TQL Stadium",
+          "text": "<p class=\"MsoNormal\">TQL Stadium is home of Major League Soccer (MLS) team FC\nCincinnati, who have played there since the stadium opened on May 16, 2021. The\nstadium holds approximately 26,000 spectators and is located in the West End\nneighborhood, at the former site of Stargel Stadium on Central Parkway at Wade\nStreet. It has a Bermuda grass surface that was installed in 2024 to replace an\nearlier hybrid grass surface.<o:p></o:p></p>"
+        }
+      ]
+    },
+    {
+      "id": "2-universityofcincinnati",
+      "name": "UniversityofCincinnati",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.8089059000327943,
+        "pitch": 0.7941225386134398,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.9758537464473864,
+          "pitch": 0.15726348240978183,
+          "title": "University of Cincinnati",
+          "text": "<p class=\"MsoNormal\">The University of Cincinnati (UC or Cincinnati, informally\nCincy) is a public research university in Cincinnati, Ohio, United States. It\nwas founded in 1819 and had an enrollment of over 53,000 students in 2024,\nmaking it the second-largest university in Ohio. It is part of the University\nSystem of Ohio.<o:p></o:p></p>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
