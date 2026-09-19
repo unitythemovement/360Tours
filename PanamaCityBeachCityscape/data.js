8bda3739acc2e58e5ee04b7a7810913844a23ddd
unitythemovement <95473103+unitythemovement@users.noreply.github.com>
2026-09-19 06:40:30 -0500

    Panama Marzipano

---
+++ PanamaCityBeachCityscape/data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_20260916064608_0044_d",
+      "name": "DJI_20260916064608_0044_D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -0.052789186238463515,
+        "pitch": 0.40493500943016336,
+        "fov": 1.1479251634083179
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.006484721799729698,
+          "pitch": 0.5610694216945298,
+          "title": "Panama City Beach, Florida",
+          "text": "<p class=\"MsoNormal\">Panama City Beach is a waterfront town and vacation\ndestination on the Emerald Coast in northwest Florida. It's known for miles of white-sand beaches\nfronting the calm, clear waters of the Gulf of Mexico.&nbsp;</p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\">St. Andrews State Park,\none of 2 protected nature preserves nearby, is bordered by the Gulf and has\nhiking trails and fishing piers. Pier Park is the main shopping hub and a venue\nfor major events.<o:p></o:p></p>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
