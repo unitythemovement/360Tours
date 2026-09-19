1540be4fd86c1dacaf5bcc933b07ffe7a8359c59
unitythemovement <95473103+unitythemovement@users.noreply.github.com>
2026-09-19 05:44:44 -0500

    Add Panama City Beach and Mobile Marzipano tours

---
+++ MobileCityscape/data.js
@@ -0,0 +1,175 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bay-of-the-holy-spirit-360",
+      "name": "Bay of the Holy Spirit 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.20595292227686635,
+        "pitch": 0.20794995347490008,
+        "fov": 1.1479251634083179
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4391179778337104,
+          "pitch": -0.09279011531648607,
+          "rotation": 3.141592653589793,
+          "target": "1-downtown-mobile-al"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.16001722004560648,
+          "pitch": 0.14211846280753093,
+          "title": "Bay of the Holy Spirit",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\">Alonso Álvarez de Pineda, during a 1519 Spanish expedition\nseeking a water passage to the Orient through the Gulf, described this harbor\nand gave it the name Bahía del Espíritu Santo.&nbsp;</span></p><p class=\"MsoNormal\"><span style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><br></span></p><p class=\"MsoNormal\"><span style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\">Successive Spanish explorers\nsought this \"Holy Grail\" along the Gulf Coast, failing until a 1693\nexpedition concluded that what is now called Mobile Bay was the fabled Bay of\nthe Holy Spirit.</span></p>"
+        },
+        {
+          "yaw": -2.146378713924058,
+          "pitch": 0.0888186653838936,
+          "title": "Jubilee Parkway (Bayway)",
+          "text": "<p class=\"MsoNormal\">The Jubilee Parkway is a pair of parallel concrete viaduct\nbridges that carry Interstate 10 across Mobile Bay from the George Wallace\nTunnel on Blakeley Island in Mobile, Alabama, eastbound to Spanish Fort/Daphne,\nAlabama.<o:p></o:p></p>"
+        },
+        {
+          "yaw": -2.9483377485554723,
+          "pitch": 0.1098803517189193,
+          "title": "Battleship Parkway (Causeway)",
+          "text": "<p class=\"MsoNormal\">Battleship Parkway is a 7-mile long causeway that carries US\n90 and US 98 eastbound across Mobile Bay from the Bankhead Tunnel on Blakeley\nIsland in Mobile, Alabama to Spanish Fort, Alabama. The roadway itself is a\nfour-lane divided highway for most of its length. It is very susceptible to\nflooding and is usually submerged during hurricane storm surges.</p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\">The Battleship\nParkway is home to Battleship Memorial Park and to many of the Mobile area's\nmore prominent seafood restaurants.<o:p></o:p></p>"
+        }
+      ]
+    },
+    {
+      "id": "1-downtown-mobile-al",
+      "name": "Downtown Mobile, AL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -2.9489358280722016,
+        "pitch": -0.07223668428642682,
+        "fov": 1.1479251634083179
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.596939306319288,
+          "pitch": 0.07846935799991073,
+          "rotation": 3.141592653589793,
+          "target": "2-ash-wednesday-2026"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.1799081030468344,
+          "pitch": 0.15729661119912208,
+          "title": "Underwater Road Tunnels",
+          "text": "The Bankhead Tunnel (US 90/98,Govt. Street) and the George Wallace Tunnel (Interstate 10) are two major underwater road tunnels passing beneath the Mobile River in downtown Mobile, AL.&nbsp;<div><br></div><div>Both tunnels connect downtown Mobile with Blakely Island.&nbsp;</div>"
+        },
+        {
+          "yaw": 2.848944272971191,
+          "pitch": -0.09758117189135618,
+          "title": "RSA Battle House Tower",
+          "text": "<p class=\"MsoNormal\">The RSA Battle House Tower is Alabama's tallest building. &nbsp;The building is owned by the Retirement\nSystems of Alabama (RSA). It is the tallest on the Gulf Coast of the United\nStates, outside Houston. It replaces the Shipt Tower in Birmingham as the\ntallest building in Alabama and the RSA Trustmark Building as the tallest in\nMobile.&nbsp;</p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\">The building is named for the neighboring Battle House Hotel, which is\nnow part of the tower complex. The Battle House Hotel was restored and\nrenovated as part of the tower project.<o:p></o:p></p>"
+        },
+        {
+          "yaw": 0.40233114247161694,
+          "pitch": 0.14446190785371016,
+          "title": "Renaissance Riverview Plaza Hotel",
+          "text": "<p class=\"MsoNormal\">Previously known as Adam's Mark Hotel, the Renaissance\nRiverview Plaza Hotel is the tallest hotel in the city and the state of\nAlabama.&nbsp; It is the 9<sup>th</sup> tallest\nbuilding in Alabama.<o:p></o:p></p><p class=\"MsoNormal\"><br></p>\n\n<p class=\"MsoNormal\">The building previously had a structural height of 277 feet;\na 2007 renovation increased the building's architectural height to 374 feet with\nthe addition of a spire.<o:p></o:p></p>"
+        }
+      ]
+    },
+    {
+      "id": "2-ash-wednesday-2026",
+      "name": "Ash Wednesday 2026",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.1603159784348982,
+        "pitch": 0.11056638972478083,
+        "fov": 1.1479251634083179
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.014672515144880549,
+          "pitch": 0.18654050527089083,
+          "title": "Ash Wednesday 2026",
+          "text": "Started by Frenchman Nicholas Langlois in 1703, when Mobile was the capital of Louisiana: beginning two and half weeks before Fat Tuesday, Mobile comes to life as the oldest Carnival celebration in the United States begins.<div><br></div><div>This experience was captured as Fat Tuesday gave way to Ash Wednesday during the early hours of February 18, February 2026.</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
